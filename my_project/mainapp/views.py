da378c72cfbdc9d47e17d95ed1574bab655f7792
Vladislav Goryakin <Happy.Seien@yandex.ru>
2022-04-28 19:17:24 +0300

    lesson-2
    progress in project
    
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- my_project/mainapp/views.py
+++ my_project/mainapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
+
+
+class ContactsView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
