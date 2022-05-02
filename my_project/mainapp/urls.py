c9a2fb09a1138d703286fce3a57ccde0ce3c3af7
Vladislav Goryakin <Happy.Seien@yandex.ru>
2022-05-02 18:54:56 +0300

    lesson-3 hw final
    final progress hw

--- my_project/mainapp/urls.py
+++ my_project/mainapp/urls.py
@@ -9,7 +9,7 @@ app_name = MainappConfig.name
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('news/', views.NewsView.as_view(), name='news'),
+    path('news/1/', views.NewsView.as_view(), name='news'),
     path('news/<int:page>/', views.NewsPaginatorView.as_view(), name='news_paginator'),
     path('newssearch/', RedirectView.as_view(url='https://yandex.ru/search/', query_string=True), name='news_search'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
